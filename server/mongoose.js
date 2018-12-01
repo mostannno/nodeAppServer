c6e8a0cdf15db0f5bea418dd44b22e4cc97adf12
tanmiao <tanmiao@BYTEDANCE.COM>
2018-12-01 19:10:45 +0800

    取消监听mongo connected事件
    Change-Id: I911285a558a64fd594e800141ec1fc70a514ffc6

--- server/mongoose.js
+++ server/mongoose.js
@@ -4,8 +4,6 @@ const DB_URL = 'mongodb://localhost:27017/manage'
 
 const init = () => {
   mongoose.connect(DB_URL, { useNewUrlParser: true });
-
-  mongoose.connection.on('connected');
   mongoose.connection.on('error', (err) => { console.log(`connect mongo err: ${err}`) });
 }
 module.exports = init;
